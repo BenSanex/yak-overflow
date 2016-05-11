cbfe0cf52bfaf734e8f4f9380cf1f2290fb02849
Ben Sanecki <ben.sanecki@gmail.com>
2016-05-11 14:43:16 -0500

    add more user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  has_many :questions
-  has_many :answers
+  has_many :posted_questions, class_name: "Question", foreign_key: :asker_id
+  has_many :posted_answers, class_name: "Answer", foreign_key: :author_id
   has_many :comments, foreign_key: :commenter_id
-  has_many :votes
+  has_many :votes, foreign_key: :voter_id
 end

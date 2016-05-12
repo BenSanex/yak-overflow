816ef21602e9cd58098024dcde665b88671ceb7b
Denny Jovic & Kevin Fowle <pair+dennyjovic.kevin-fowle@devbootcamp.com>
2016-05-12 11:50:16 -0500

    working js for answer votes

--- app/controllers/votes.rb
+++ app/controllers/votes.rb
@@ -25,3 +25,21 @@ post '/questions/:id/downvote' do
     redirect "/questions/#{params[:id]}"
   end
 end
+
+post '/answers/:id/upvote' do
+  Vote.create(votable_id: params[:id], votable_type: :Answer, voter_id: session[:user_id], direction: :up)
+  if request.xhr?
+    "this doesn't matter"
+  else
+    redirect "/questions/#{params[:question_id]}"
+  end
+end
+
+post '/answers/:id/downvote' do
+  Vote.create(votable_id: params[:id], votable_type: :Answer, voter_id: session[:user_id], direction: :down)
+  if request.xhr?
+   "this doesn't matter"
+  else
+    redirect "/questions/#{params[:question_id]}"
+  end
+end
--- public/js/application.js
+++ public/js/application.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   // upvote
   $(".upvote").on("submit", function(event){
     event.preventDefault();
-    var data = $(".total").text()
+    var data = $(".total").text();
     $(".total").text(parseInt(data) + 1);
     var action = $(this).attr("action");
 
@@ -15,7 +15,7 @@ $(document).ready(function() {
   // downvote
   $(".downvote").on("submit", function(event){
     event.preventDefault();
-    var data = $(".total").text()
+    var data = $(".total").text();
     // html visual
     $(".total").text(parseInt(data) - 1);
     var action = $(this).attr("action");
@@ -25,4 +25,34 @@ $(document).ready(function() {
       url: action
     });
   });
+
+    // upvote
+  $(".aupvote").on("submit", function(event){
+    event.preventDefault();
+    var data = $(this).parent().find(".atotal").text();
+    // html visual
+    $(this).parent().find(".atotal").text(parseInt($(this).parent().find(".atotal").text()) + 1);
+
+    var action = $(this).attr("action");
+
+    $.ajax({
+      method: "post",
+      url: action
+    });
+  });
+
+  // downvote
+  $(".adownvote").on("submit", function(event){
+    event.preventDefault();
+    var data = $(this).parent().find(".atotal").text();
+    // html visual
+    $(this).parent().find(".atotal").text(parseInt($(this).parent().find(".atotal").text()) - 1);
+    var action = $(this).attr("action");
+
+    $.ajax({
+      method: "post",
+      url: action
+    });
+  });
+
 });
